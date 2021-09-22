47446fc09ce9193b714bbf875de347161c1dba32
haiold <anb190621@student.ctu.edu.vn>
2021-09-22 23:04:14 +0700

    Cai dat xu ly loi

--- app/helpers/errors.js
+++ app/helpers/errors.js
@@ -1,15 +1,14 @@
-const handle = (Promise) => {
-    return Promise.then((data) => [null, data]).catch((Error) => [error, undefined]);
-};
+const handle = promise => {
+    return promise.then(data=>[null,data]).catch(error=>[error,undefined]);
 
-class BadRequestError extends Error {
-    constructor(statusCode, message) {
+};
+class BadRequestError extends Error{
+    constructor(statusCode,message){
         super();
         this.statusCode = statusCode;
         this.message = message;
     }
 }
-
 module.exports = {
     handle,
     BadRequestError,
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError } = require("./app/helpers/errors");
+
 const app  =express();
 
 app.use(cors({origin: config.app.origins}));
@@ -9,14 +11,25 @@ app.use(cors({origin: config.app.origins}));
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
-
-
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+    });
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+    });
+app.use((err, req, res, next) => {
+console.log(err);
+res.status(err.statusCode || 500).json({
+message: err.message || "Internal Server Error",
+});
+});
 app.get("/",(req, res)=>{
     res.json({message: "Welcom  contact book appliction."});
 });
-
 setupContactRoutes(app);
-
 const PORT = config.app.port;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}.`);
